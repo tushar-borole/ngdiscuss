3c2700a9c4553bee2c1cd0564e6df3c975bb1179
Tushar Borole <tushariscoolster@gmail.com>
2015-06-21 20:27:41 +0530

    Blog chages

--- app/scripts/filter/filters.js
+++ app/scripts/filter/filters.js
@@ -5,7 +5,10 @@ Copyright:Karma Worldwide Inc 2014*/
 /*eg arrayOfObjectswithKeys | filterMultiple:{key1:['value1','value2','value3',...etc],key2:'value4',key3:[value5,value6,...etc]}*/
 app.filter('split', ['$filter', function ($filter) {
     return function (items, split) {
-        var array = items.split(split);
+        var array=[];
+        if(angular.isDefined(items)){
+        array = items.split(split);
+        }
         return array;
     }
 }]);
